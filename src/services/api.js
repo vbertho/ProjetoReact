51f887a8a8853807b022436b3113e395b44dd54d
Vinicius <viniciusberthao@gmail.com>
2026-03-09 12:31:53 -0300

    fix: changed api URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: "https://cookiehub.onrender.com",
-    baseURL: "http://localhost:8080",
+    baseURL: "https://cookiehub.onrender.com",
+    //baseURL: "http://localhost:8080",
 });
 
 // sends the token automatically in every request
